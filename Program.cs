dda1b0318baa2149f4d669cc029883c9ecbccee5
Leivi Jones (1000142103) <JONELS1@student.op.ac.nz>
2026-09-22 10:40:36 +1200

    program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace studio_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
