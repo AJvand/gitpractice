bbc41dc97f4e3f39d1da758fdb46fe0be8dec225
Antony van der Schuit (1000144772) <VANDAJ8@student.op.ac.nz>
2026-09-22 11:08:48 +1200

    relocated

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,10 +1,167 @@
-﻿namespace practice
+﻿namespace studio_test
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Niko's Lab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // AJ's Lab
+            {
+                Random rand = new Random();
+                double guessTarget = 0;
+                double userGuess = -1;
+                double attempts = 0;
+                string temp = "AAAAAAAAAAAAAAA";
+
+                guessTarget = rand.Next(1, 101);
+                Console.WriteLine("Guess a number between 1 and 100");
+                temp = Console.ReadLine();
+                userGuess = Convert.ToDouble(temp);
+                do
+                {
+                    if (userGuess > guessTarget)
+                    {
+                        if (attempts == 6)
+                        {
+                            Console.WriteLine("This is taking a lot of attempts, you should try a better strategy");
+                        }
+                        Console.WriteLine("You guessed too high, try again");
+                        Thread.Sleep(800);
+                        attempts = (attempts + 1);
+                        Console.WriteLine("Input your guess");
+                        temp = Console.ReadLine();
+                        Thread.Sleep(800);
+                        userGuess = Convert.ToDouble(temp);
+                    }
+
+                    else if (userGuess < guessTarget)
+                    {
+                        if (attempts == 6)
+                        {
+                            Console.WriteLine("This is taking a lot of attempts, you should try a better strategy");
+                        }
+                        Console.WriteLine("You guessed too low, try again");
+                        Thread.Sleep(800);
+                        attempts = (attempts + 1);
+                        Console.WriteLine("Input your guess");
+                        temp = Console.ReadLine();
+                        Thread.Sleep(800);
+                        userGuess = Convert.ToDouble(temp);
+                    }
+                } while (userGuess != guessTarget);
+                Thread.Sleep(800);
+                Console.WriteLine("Congratulations, you guessed the right number!");
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // Leivi's Lab
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("*********************************************************");
+            Console.WriteLine("*   Leivi                                               *");
+            Console.WriteLine("*   1 Nile St                                           *");
+            Console.WriteLine("*   My favourite BIT subject is : Programming           *");
+            Console.WriteLine("*                                                       *");
+            Console.WriteLine("*********************************************************");
+            Console.ReadLine();
         }
     }
 }
